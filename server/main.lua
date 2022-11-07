814a2cb4b1ab4e24920e8f99d8dc8c70abff3a22
oosayeroo <98313914+oosayeroo@users.noreply.github.com>
2022-11-07 19:59:06 +0000

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,13 @@
+local npcsspawned = false
+
+Citizen.CreateThread(function()
+    RegisterServerEvent("collectnpcs")
+    AddEventHandler("collectnpcs", function()
+        if npcsspawned == false then
+			local _source = source
+			TriggerClientEvent("spawnnpcs", _source)
+			npcsspawned = true
+		end
+    end)
+end)
 
